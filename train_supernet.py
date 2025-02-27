b07861f8769ef8fec485f478e8540f4eebc2f265
Jérémy Morlier <jeremy.morlier@imt-atlantique.fr>
2025-02-27 16:25:54 +0100

    find_unused_parameters=True in torch ddp

--- train_supernet.py
+++ train_supernet.py
@@ -289,7 +289,7 @@ def main(args) :
     # Parallelize the model using Distributed Data Parallel (DDP)
     model_without_ddp = model
     if args.distributed:
-        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
         model_without_ddp = model.module
     
 
