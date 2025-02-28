0ed501f9741695ca506143e1a44654a221685f68
Jérémy Morlier <jeremy.morlier@imt-atlantique.fr>
2025-02-28 11:18:00 +0100

    fix slurm_script

--- slurm/test_training.sh
+++ slurm/test_training.sh
@@ -1,15 +1,14 @@
-
-#SBATCH --account             sxq@v100
-#SBATCH --constraint          v100-32g
+#SBATCH --account             sxq@a100
+#SBATCH --constraint          a100
 #SBATCH --cpus-per-task       10
 #SBATCH --error               log/%x/%j/errors.err
 #SBATCH --gres                gpu:4
 #SBATCH --hint                nomultithread
-#SBATCH --job-name            3_ResNet
+#SBATCH --job-name            HWSuper_Test
 #SBATCH --nodes               1
 #SBATCH --ntasks              4
 #SBATCH --output              log/%x/%j/logs.out
-#SBATCH --qos                 qos_gpu-t3
+#SBATCH --qos                 qos_gpu_a100-t3 
 #SBATCH --time                20:00:00
 #SBATCH --signal              USR1@40
 
