569d2dc8b01d7fa97bd9d6cba1f59818a03055f3
Jérémy Morlier <jeremy.morlier@imt-atlantique.fr>
2025-07-30 14:45:35 +0200

    added evolutionary search files

---
+++ slurm/evo/evo5.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account             sxq@a100
+#SBATCH --constraint          a100
+#SBATCH --cpus-per-task       10
+#SBATCH --error               log/%x/%j/errors.err
+#SBATCH --gres                gpu:4
+#SBATCH --hint                nomultithread
+#SBATCH --job-name            HWSuper_Test
+#SBATCH --nodes               1
+#SBATCH --ntasks              4
+#SBATCH --output              log/%x/%j/logs.out
+#SBATCH --qos                 qos_gpu_a100-t3 
+#SBATCH --time                20:00:00
+#SBATCH --signal              USR1@40
+
+# This experiment is the Autoformer SuperNet Small with an evolutionary search of 18M to 23M
+module purge
+conda deactivate
+
+module load arch/a100
+source .venv/bin/activate
+
+srun python3 evolution_subnet.py --data-path $DSDIR/imagenet --output_dir results/evoS18m23m --gp --change_qk --relative_position --dist-eval --cfg config/supernet-S.yaml --resume checkpoints/supernet-small.pth --min-param-limits 18 --param-limits 23 --data-path $DSDIR/imagenet --evo-data-path $SCRATCH/EvoImageNet/ --data-set EVO_IMNET
